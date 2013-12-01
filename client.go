6700063551d89efe9f55fb5feb9145b16ac166b9
Oliver Eilhard <oliver@eilhard.net>
2013-12-01 14:20:30 +0100

    Connection Pool
    Added a very simple connection pool. Users can now specify one or
    more Elasticsearch server URLs when creating a Client via NewClient.
    
    Example:
    
      client, err := NewClient(http.DefaultClient,
        "http://127.0.0.1:9200", "http://127.0.0.2:9201")
    
    The client then starts up and periodically checks for an available
    connection. It takes the first connection available and uses it
    for subsequent requests. The interval for periodic checks is currently
    1 minute.
    
    In case no active Elasticsearch server is found, the client will
    return ErrNoClient when creating a new request via NewRequest.

--- client.go
+++ client.go
@@ -6,52 +6,135 @@ package elastic
 
 import (
 	"errors"
+	"log"
 	"net/http"
+	"net/url"
+	"sync"
+	"time"
 )
 
-// Current version.
-const Version = "0.1"
+const (
+	// Current version.
+	Version = "0.2"
 
-// Default URL to be used as base for ElasticSearch requests.
-const defaultUrl = "http://localhost:9200"
+	// defaultUrl to be used as base for Elasticsearch requests.
+	defaultUrl = "http://localhost:9200"
 
-// An ElasticSearch client.
+	// pingDuration is the time to periodically check the Elasticsearch URLs.
+	pingDuration = 60 * time.Second
+)
+
+var (
+	// ErrNoClient is raised when no active Elasticsearch client is available.
+	ErrNoClient = errors.New("no active client")
+)
+
+// Client is an ElasticSearch client. Create one by calling NewClient.
 type Client struct {
-	// Base URL for ElasticSearch queries
-	Url string
+	urls []string // urls is a list of all clients for ElasticSearch queries
 
-	c *http.Client
+	c *http.Client // c is the net/http Client to use for requests
+
+	mu        sync.Mutex // mutex for the next two fields
+	activeUrl string     // currently active connection url
+	hasActive bool       // true if we have an active connection
 }
 
 // Creates a new client to work with ElasticSearch.
-func NewClient(client *http.Client) (*Client, error) {
+func NewClient(client *http.Client, urls ...string) (*Client, error) {
 	if client == nil {
 		return nil, errors.New("client is nil")
 	}
-	c := &Client{Url: defaultUrl, c: client}
+	c := &Client{c: client}
+	switch len(urls) {
+	case 0:
+		c.urls = make([]string, 1)
+		c.urls[0] = defaultUrl
+	case 1:
+		c.urls = make([]string, 1)
+		c.urls[0] = urls[0]
+	default:
+		c.urls = urls
+	}
+	c.pingUrls()
+	go c.pinger() // start goroutine periodically ping all clients
 	return c, nil
 }
 
-// Helper function that adds e.g. the base URL to the request.
+// NewRequest creates a new request with the given method and prepends
+// the base URL to the path. If no active connection to Elasticsearch
+// is available, ErrNoClient is returned.
 func (c *Client) NewRequest(method, path string) (*Request, error) {
-	return NewRequest(method, c.Url+path)
+	if !c.hasActive {
+		return nil, ErrNoClient
+	}
+	return NewRequest(method, c.activeUrl+path)
 }
 
-// Create a new index.
+// pinger periodically runs pingUrls.
+func (c *Client) pinger() {
+	ticker := time.NewTicker(pingDuration)
+	for {
+		select {
+		case <-ticker.C:
+			c.pingUrls()
+		}
+	}
+}
+
+// pingUrls iterates through all client URLs. It checks if the client
+// is available. It takes the first one available and saves its URL
+// in activeUrl. If no client is available, hasActive is set to false
+// and NewRequest will fail.
+func (c *Client) pingUrls() {
+	for _, url_ := range c.urls {
+		params := make(url.Values)
+		params.Set("timeout", "1")
+		req, err := NewRequest("HEAD", url_+"/?"+params.Encode())
+		if err == nil {
+			res, err := c.c.Do((*http.Request)(req))
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode == http.StatusOK {
+					// Everything okay: Update activeUrl and set hasActive to true.
+					c.mu.Lock()
+					defer c.mu.Unlock()
+					if c.activeUrl != "" && c.activeUrl != url_ {
+						log.Printf("elastic: switched connection from %s to %s", c.activeUrl, url_)
+					}
+					c.activeUrl = url_
+					c.hasActive = true
+					return
+				}
+			} else {
+				log.Printf("elastic: %v", err)
+			}
+		} else {
+			log.Printf("elastic: %v", err)
+		}
+	}
+
+	// No client available
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.hasActive = false
+}
+
+// CreateIndex starts the service to create a new index.
 func (c *Client) CreateIndex(name string) *CreateIndexService {
 	builder := NewCreateIndexService(c)
 	builder.Index(name)
 	return builder
 }
 
-// Delete an index.
+// DeleteIndex starts the service to delete an index.
 func (c *Client) DeleteIndex(name string) *DeleteIndexService {
 	builder := NewDeleteIndexService(c)
 	builder.Index(name)
 	return builder
 }
 
-// Check if an index exists.
+// IndexExists allows to check if an index exists.
 func (c *Client) IndexExists(name string) *IndexExistsService {
 	builder := NewIndexExistsService(c)
 	builder.Index(name)
@@ -70,7 +153,7 @@ func (c *Client) Delete() *DeleteService {
 	return builder
 }
 
-// Delete documents by query.
+// DeleteByQuery deletes documents as found by a query.
 func (c *Client) DeleteByQuery() *DeleteByQueryService {
 	builder := NewDeleteByQueryService(c)
 	return builder
@@ -82,7 +165,7 @@ func (c *Client) Get() *GetService {
 	return builder
 }
 
-// Check if a document exists.
+// Exists checks if a document exists.
 func (c *Client) Exists() *ExistsService {
 	builder := NewExistsService(c)
 	return builder
@@ -95,7 +178,7 @@ func (c *Client) Count(indices ...string) *CountService {
 	return builder
 }
 
-// Start a search.
+// Search is the entry point for searches.
 func (c *Client) Search(indices ...string) *SearchService {
 	builder := NewSearchService(c)
 	builder.Indices(indices...)
@@ -116,25 +199,26 @@ func (c *Client) Scan(indices ...string) *ScanService {
 	return builder
 }
 
-// Flush.
+// Flush asks Elasticsearch to free memory from the index and
+// flush data to disk.
 func (c *Client) Flush() *FlushService {
 	builder := NewFlushService(c)
 	return builder
 }
 
-// Bulk.
+// Bulk is the entry point to mass insert/update/delete documents.
 func (c *Client) Bulk() *BulkService {
 	builder := NewBulkService(c)
 	return builder
 }
 
-// Add and remove aliases.
+// Alias enables the caller to add and/or remove aliases.
 func (c *Client) Alias() *AliasService {
 	builder := NewAliasService(c)
 	return builder
 }
 
-// Get aliases by index name(s).
+// Aliases returns aliases by index name(s).
 func (c *Client) Aliases() *AliasesService {
 	builder := NewAliasesService(c)
 	return builder
---
+++ client_test.go
@@ -0,0 +1,74 @@
+package elastic
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestSingleUrl(t *testing.T) {
+	client, err := NewClient(http.DefaultClient)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(client.urls) != 1 {
+		t.Fatalf("expected 1 default client url, got: %v", client.urls)
+	}
+	if client.urls[0] != defaultUrl {
+		t.Errorf("expected default client url of %s, got: %s", defaultUrl, client.urls[0])
+	}
+}
+
+func TestMultipleUrls(t *testing.T) {
+	client, err := NewClient(http.DefaultClient, "http://localhost:9200", "http://localhost:9201")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(client.urls) != 2 {
+		t.Fatalf("expected 2 default client urls, got: %v", client.urls)
+	}
+	if client.urls[0] != "http://localhost:9200" {
+		t.Errorf("expected 1st client url of %s, got: %s", "http://localhost:9200", client.urls[0])
+	}
+	if client.urls[1] != "http://localhost:9201" {
+		t.Errorf("expected 2nd client url of %s, got: %s", "http://localhost:9201", client.urls[0])
+	}
+}
+
+func TestFindingActiveClient(t *testing.T) {
+	client, err := NewClient(http.DefaultClient, "http://localhost:9201", "http://localhost:9200")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(client.urls) != 2 {
+		t.Fatalf("expected 2 default client urls, got: %v", client.urls)
+	}
+	if !client.hasActive {
+		t.Errorf("expected to have active connection, got: %v", client.hasActive)
+	}
+	if client.activeUrl != "http://localhost:9200" {
+		t.Errorf("expected active url to be http://localhost:9200, got: %v", client.activeUrl)
+	}
+}
+
+func TestFindingNoActiveClient(t *testing.T) {
+	client, err := NewClient(http.DefaultClient, "http://localhost:9201", "http://localhost:9202")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(client.urls) != 2 {
+		t.Fatalf("expected 2 default client urls, got: %v", client.urls)
+	}
+	if client.hasActive {
+		t.Errorf("expected to not have an active connection, got: %v", client.hasActive)
+	}
+	if client.activeUrl != "" {
+		t.Errorf("expected no active url, got: %v", client.activeUrl)
+	}
+	req, err := client.NewRequest("HEAD", "/")
+	if err != ErrNoClient {
+		t.Errorf("expected ErrNoClient, got: %v", err)
+	}
+	if req != nil {
+		t.Errorf("expected no request, got: %v", req)
+	}
+}
