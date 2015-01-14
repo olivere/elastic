177e9b0026d7823682fa6aa74f6baf3bc4f7cb77
Oliver Eilhard <oliver@eilhard.net>
2015-01-14 09:09:44 +0100

    Fix issue with mixing different sort methods
    Sorting was wrong when one mixed the different sort methods, e.g.
    Sort(), SortWithInfo(), and SortBy(). It's unified now.
    
    Closes #31.

--- search_source_test.go
+++ search_source_test.go
@@ -168,3 +168,20 @@ func TestSearchSourceIndexBoost(t *testing.T) {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
 }
+
+func TestSearchSourceMixDifferentSorters(t *testing.T) {
+	matchAllQ := NewMatchAllQuery()
+	builder := NewSearchSource().Query(matchAllQ).
+		Sort("a", false).
+		SortWithInfo(SortInfo{Field: "b", Ascending: true}).
+		SortBy(NewScriptSort("doc['field_name'].value * factor", "number").Param("factor", 1.1))
+	data, err := json.Marshal(builder.Source())
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"query":{"match_all":{}},"sort":[{"a":{"order":"desc"}},{"b":{"order":"asc"}},{"_script":{"params":{"factor":1.1},"script":"doc['field_name'].value * factor","type":"number"}}]}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
