8c9c72ef207f43741787de4e1946fa9ab06410e0
Oliver Eilhard <oliver@eilhard.net>
2015-11-04 09:19:19 +0100

    Add missing properties to Count API
    The Count service was missing a few properties from the REST API.
    
    Closes #158.

--- count_test.go
+++ count_test.go
@@ -6,6 +6,47 @@ package elastic
 
 import "testing"
 
+func TestCountURL(t *testing.T) {
+	client := setupTestClientAndCreateIndex(t)
+
+	tests := []struct {
+		Indices  []string
+		Types    []string
+		Expected string
+	}{
+		{
+			[]string{},
+			[]string{},
+			"/_all/_count",
+		},
+		{
+			[]string{},
+			[]string{"tweet"},
+			"/_all/tweet/_count",
+		},
+		{
+			[]string{"twitter-*"},
+			[]string{"tweet", "follower"},
+			"/twitter-%2A/tweet%2Cfollower/_count",
+		},
+		{
+			[]string{"twitter-2014", "twitter-2015"},
+			[]string{"tweet", "follower"},
+			"/twitter-2014%2Ctwitter-2015/tweet%2Cfollower/_count",
+		},
+	}
+
+	for _, test := range tests {
+		path, _, err := client.Count().Indices(test.Indices...).Types(test.Types...).buildURL()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if path != test.Expected {
+			t.Errorf("expected %q; got: %q", test.Expected, path)
+		}
+	}
+}
+
 func TestCount(t *testing.T) {
 	client := setupTestClientAndCreateIndex(t)
 
