7381948817fe357d523dc2e0594502af25aa1786
SeUkKim <eleccookie@gmail.com>
2016-01-04 20:39:54 +0900

    fix sniffing for ES managed by AWS
    ES managed by AWS does not support sniffing = true by default.

--- client.go
+++ client.go
@@ -51,7 +51,8 @@ const (
 	DefaultHealthcheckInterval = 60 * time.Second
 
 	// DefaultSnifferEnabled specifies if the sniffer is enabled by default.
-	DefaultSnifferEnabled = true
+	// ES managed by AWS does not support sniffing = true by default.
+	DefaultSnifferEnabled = false
 
 	// DefaultSnifferInterval is the interval between two sniffing procedures,
 	// i.e. the lookup of all nodes in the cluster and their addition/removal
