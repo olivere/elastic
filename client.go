ea60f14deda82f645e0221894b3a9af6b0c76819
Oliver Eilhard <oliver@eilhard.net>
2016-03-03 21:03:57 +0100

    Correctly pass requests in After callback
    The BulkProcessor now correctly passes the requests to the After
    callback.

--- client.go
+++ client.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "3.0.23"
+	Version = "3.0.24"
 
 	// DefaultUrl is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
