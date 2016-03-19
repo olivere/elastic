e713be938499714ee063069b868040d39c96cffd
Oliver Eilhard <oliver@eilhard.net>
2016-03-19 15:06:38 +0100

    Don't trim path in canonicalize
    This commit allows to connect to nodes that do not run at the base URL.
    The official clients support this as well. See #243 and #244.

--- canonicalize.go
+++ canonicalize.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2015 Oliver Eilhard. All rights reserved.
+// Copyright 2012-present Oliver Eilhard. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
@@ -7,19 +7,23 @@ package elastic
 import "net/url"
 
 // canonicalize takes a list of URLs and returns its canonicalized form, i.e.
-// remove anything but scheme, userinfo, host, and port. It also removes the
-// slash at the end. It also skips invalid URLs or URLs that do not use
-// protocol http or https.
+// remove anything but scheme, userinfo, host, path, and port.
+// It also removes all trailing slashes. It also skips invalid URLs or
+// URLs that do not use protocol http or https.
 //
 // Example:
-// http://127.0.0.1:9200/path?query=1 -> http://127.0.0.1:9200
+// http://127.0.0.1:9200/?query=1 -> http://127.0.0.1:9200
+// http://127.0.0.1:9200/db1/ -> http://127.0.0.1:9200/db1
 func canonicalize(rawurls ...string) []string {
 	canonicalized := make([]string, 0)
 	for _, rawurl := range rawurls {
 		u, err := url.Parse(rawurl)
 		if err == nil && (u.Scheme == "http" || u.Scheme == "https") {
+			// Trim trailing slashes
+			for len(u.Path) > 0 && u.Path[len(u.Path)-1] == '/' {
+				u.Path = u.Path[0 : len(u.Path)-1]
+			}
 			u.Fragment = ""
-			u.Path = ""
 			u.RawQuery = ""
 			canonicalized = append(canonicalized, u.String())
 		}
