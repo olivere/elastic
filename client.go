2594d14da9d16fb44b8153d2db83dbea04b504e8
Oliver Eilhard <oliver@eilhard.net>
2016-04-16 17:04:54 +0200

    Release 3.0.31
    - Script heuristics for significant terms agg
    - Indices options for search service
    - Fix timer leaks in client

--- client.go
+++ client.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "3.0.30"
+	Version = "3.0.31"
 
 	// DefaultUrl is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
