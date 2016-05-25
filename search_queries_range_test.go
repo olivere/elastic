983a7fb72489902c0ab51f71f9a6f11dc56337e5
Oliver Eilhard <oliver@eilhard.net>
2016-05-25 09:32:24 +0200

    Move boost parameter in range query
    The `boost` parameter was in the wrong position in the query
    structure.
    
    See #290
    See #291

--- search_queries_range_test.go
+++ search_queries_range_test.go
@@ -10,33 +10,18 @@ import (
 )
 
 func TestRangeQuery(t *testing.T) {
-	q := NewRangeQuery("postDate").From("2010-03-01").To("2010-04-01")
+	q := NewRangeQuery("postDate").From("2010-03-01").To("2010-04-01").Boost(3)
 	q = q.QueryName("my_query")
 	data, err := json.Marshal(q.Source())
 	if err != nil {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"range":{"_name":"my_query","postDate":{"from":"2010-03-01","include_lower":true,"include_upper":true,"to":"2010-04-01"}}}`
+	expected := `{"range":{"_name":"my_query","postDate":{"boost":3,"from":"2010-03-01","include_lower":true,"include_upper":true,"to":"2010-04-01"}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
 }
-
-/*
-func TestRangeQueryGte(t *testing.T) {
-	q := NewRangeQuery("postDate").Gte("2010-03-01")
-	data, err := json.Marshal(q.Source())
-	if err != nil {
-		t.Fatalf("marshaling to JSON failed: %v", err)
-	}
-	got := string(data)
-	expected := `{"range":{"postDate":{"gte":"2010-03-01"}}}`
-	if got != expected {
-		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
-	}
-}
-*/
 
 func TestRangeQueryWithTimeZone(t *testing.T) {
 	f := NewRangeQuery("born").
