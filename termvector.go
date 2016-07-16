94006d2635f2304b61577a8d7757cf4b4d48cff7
Oliver Eilhard <oliver@eilhard.net>
2016-07-16 12:05:20 +0200

    Add missing `score` field in termvectors

--- termvector.go
+++ termvector.go
@@ -292,6 +292,7 @@ type TokenInfo struct {
 
 type TermsInfo struct {
 	DocFreq  int64       `json:"doc_freq"`
+	Score    float64     `json:"score"`
 	TermFreq int64       `json:"term_freq"`
 	Ttf      int64       `json:"ttf"`
 	Tokens   []TokenInfo `json:"tokens"`
