d698c70a808002621d8b5689b3aed0bd2630e8b4
John Stanford <jxstanford@gmail.com>
2016-07-29 07:46:31 -0700

    Adds CustomQuery type that is an extension of string with a Source method

---
+++ search_queries_custom.go
@@ -0,0 +1,25 @@
+// Copyright 2012-2015 Oliver Eilhard, John Stanford. All rights reserved.
+// Use of this source code is governed by a MIT-license.
+// See http://olivere.mit-license.org/license.txt for details.
+
+package elastic
+
+import "encoding/json"
+
+// CustomQuery is a user provided query
+//
+// For more details, see
+// https://www.elastic.co/guide/en/elasticsearch/reference/master/query-dsl-custom-query.html
+
+// CustomQuery can be used to treat a string representation of an ES query
+// as a Query.  Example usage:
+//    q := CustomQuery("{\"query\":{\"match_all\":{}}}")
+//    db.Search().Query(q).From(1).Size(100).Do()
+type CustomQuery string
+
+// Source returns the JSON encoded body
+func (q CustomQuery) Source() (interface{}, error) {
+	var f interface{}
+	err := json.Unmarshal([]byte(q), &f)
+	return f, err
+}
