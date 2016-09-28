fe502785e39e78aec342eb77c613725562beba59
Oliver Eilhard <oliver@eilhard.net>
2016-09-28 11:02:08 +0200

    Move `request_cache` into query params
    According to the documentation, `request_cache` is a parameter to be
    passed in the query string, not in the body.
    
    See #353 and #365

--- search.go
+++ search.go
@@ -305,6 +305,9 @@ func (s *SearchService) buildURL() (string, url.Values, error) {
 	if s.preference != "" {
 		params.Set("preference", s.preference)
 	}
+	if s.searchSource != nil && s.searchSource.requestCache != nil {
+		params.Set("request_cache", fmt.Sprintf("%v", *s.searchSource.requestCache))
+	}
 	if s.allowNoIndices != nil {
 		params.Set("allow_no_indices", fmt.Sprintf("%v", *s.allowNoIndices))
 	}
