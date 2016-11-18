683972b9c90e84e22fdd9d9478fad0219d7cf47c
Oliver Eilhard <oliver@eilhard.net>
2016-11-18 14:09:56 +0100

    Fix Multi GET API
    There are a few changes to the Multi GET API. E.g. the `fields` property
    is now `stored_fields`. Also added a few properties that were missing
    from the REST API spec.
    
    Close #389

--- mget.go
+++ mget.go
@@ -7,6 +7,7 @@ package elastic
 import (
 	"fmt"
 	"net/url"
+	"strings"
 
 	"golang.org/x/net/context"
 )
@@ -16,38 +17,56 @@ import (
 // a docs array with all the fetched documents, each element similar
 // in structure to a document provided by the Get API.
 //
-// See https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-multi-get.html
+// See https://www.elastic.co/guide/en/elasticsearch/reference/5.0/docs-multi-get.html
 // for details.
 type MgetService struct {
-	client     *Client
-	pretty     bool
-	preference string
-	realtime   *bool
-	refresh    string
-	items      []*MultiGetItem
+	client       *Client
+	pretty       bool
+	preference   string
+	realtime     *bool
+	refresh      string
+	routing      string
+	storedFields []string
+	items        []*MultiGetItem
 }
 
+// NewMgetService initializes a new Multi GET API request call.
 func NewMgetService(client *Client) *MgetService {
 	builder := &MgetService{
 		client: client,
-		items:  make([]*MultiGetItem, 0),
 	}
 	return builder
 }
 
-func (b *MgetService) Preference(preference string) *MgetService {
-	b.preference = preference
-	return b
+// Preference specifies the node or shard the operation should be performed
+// on (default: random).
+func (s *MgetService) Preference(preference string) *MgetService {
+	s.preference = preference
+	return s
 }
 
-func (b *MgetService) Refresh(refresh string) *MgetService {
-	b.refresh = refresh
-	return b
+// Refresh the shard containing the document before performing the operation.
+func (s *MgetService) Refresh(refresh string) *MgetService {
+	s.refresh = refresh
+	return s
 }
 
-func (b *MgetService) Realtime(realtime bool) *MgetService {
-	b.realtime = &realtime
-	return b
+// Realtime specifies whether to perform the operation in realtime or search mode.
+func (s *MgetService) Realtime(realtime bool) *MgetService {
+	s.realtime = &realtime
+	return s
+}
+
+// Routing is the specific routing value.
+func (s *MgetService) Routing(routing string) *MgetService {
+	s.routing = routing
+	return s
+}
+
+// StoredFields is a list of fields to return in the response.
+func (s *MgetService) StoredFields(storedFields ...string) *MgetService {
+	s.storedFields = append(s.storedFields, storedFields...)
+	return s
 }
 
 // Pretty indicates that the JSON response be indented and human readable.
@@ -56,15 +75,17 @@ func (s *MgetService) Pretty(pretty bool) *MgetService {
 	return s
 }
 
-func (b *MgetService) Add(items ...*MultiGetItem) *MgetService {
-	b.items = append(b.items, items...)
-	return b
+// Add an item to the request.
+func (s *MgetService) Add(items ...*MultiGetItem) *MgetService {
+	s.items = append(s.items, items...)
+	return s
 }
 
-func (b *MgetService) Source() (interface{}, error) {
+// Source returns the request body, which will be serialized into JSON.
+func (s *MgetService) Source() (interface{}, error) {
 	source := make(map[string]interface{})
-	items := make([]interface{}, len(b.items))
-	for i, item := range b.items {
+	items := make([]interface{}, len(s.items))
+	for i, item := range s.items {
 		src, err := item.Source()
 		if err != nil {
 			return nil, err
@@ -75,36 +96,43 @@ func (b *MgetService) Source() (interface{}, error) {
 	return source, nil
 }
 
-func (b *MgetService) Do(ctx context.Context) (*MgetResponse, error) {
+// Do executes the request.
+func (s *MgetService) Do(ctx context.Context) (*MgetResponse, error) {
 	// Build url
 	path := "/_mget"
 
 	params := make(url.Values)
-	if b.realtime != nil {
-		params.Add("realtime", fmt.Sprintf("%v", *b.realtime))
+	if s.realtime != nil {
+		params.Add("realtime", fmt.Sprintf("%v", *s.realtime))
 	}
-	if b.preference != "" {
-		params.Add("preference", b.preference)
+	if s.preference != "" {
+		params.Add("preference", s.preference)
 	}
-	if b.refresh != "" {
-		params.Add("refresh", b.refresh)
+	if s.refresh != "" {
+		params.Add("refresh", s.refresh)
+	}
+	if s.routing != "" {
+		params.Set("routing", s.routing)
+	}
+	if len(s.storedFields) > 0 {
+		params.Set("stored_fields", strings.Join(s.storedFields, ","))
 	}
 
 	// Set body
-	body, err := b.Source()
+	body, err := s.Source()
 	if err != nil {
 		return nil, err
 	}
 
 	// Get response
-	res, err := b.client.PerformRequest(ctx, "GET", path, params, body)
+	res, err := s.client.PerformRequest(ctx, "GET", path, params, body)
 	if err != nil {
 		return nil, err
 	}
 
 	// Return result
 	ret := new(MgetResponse)
-	if err := b.client.decoder.Decode(res.Body, ret); err != nil {
+	if err := s.client.decoder.Decode(res.Body, ret); err != nil {
 		return nil, err
 	}
 	return ret, nil
@@ -114,45 +142,48 @@ func (b *MgetService) Do(ctx context.Context) (*MgetResponse, error) {
 
 // MultiGetItem is a single document to retrieve via the MgetService.
 type MultiGetItem struct {
-	index       string
-	typ         string
-	id          string
-	routing     string
-	fields      []string
-	version     *int64 // see org.elasticsearch.common.lucene.uid.Versions
-	versionType string // see org.elasticsearch.index.VersionType
-	fsc         *FetchSourceContext
+	index        string
+	typ          string
+	id           string
+	routing      string
+	storedFields []string
+	version      *int64 // see org.elasticsearch.common.lucene.uid.Versions
+	versionType  string // see org.elasticsearch.index.VersionType
+	fsc          *FetchSourceContext
 }
 
+// NewMultiGetItem initializes a new, single item for a Multi GET request.
 func NewMultiGetItem() *MultiGetItem {
 	return &MultiGetItem{}
 }
 
+// Index specifies the index name.
 func (item *MultiGetItem) Index(index string) *MultiGetItem {
 	item.index = index
 	return item
 }
 
+// Type specifies the type name.
 func (item *MultiGetItem) Type(typ string) *MultiGetItem {
 	item.typ = typ
 	return item
 }
 
+// Id specifies the identifier of the document.
 func (item *MultiGetItem) Id(id string) *MultiGetItem {
 	item.id = id
 	return item
 }
 
+// Routing is the specific routing value.
 func (item *MultiGetItem) Routing(routing string) *MultiGetItem {
 	item.routing = routing
 	return item
 }
 
-func (item *MultiGetItem) Fields(fields ...string) *MultiGetItem {
-	if item.fields == nil {
-		item.fields = make([]string, 0)
-	}
-	item.fields = append(item.fields, fields...)
+// StoredFields is a list of fields to return in the response.
+func (item *MultiGetItem) StoredFields(storedFields ...string) *MultiGetItem {
+	item.storedFields = append(item.storedFields, storedFields...)
 	return item
 }
 
@@ -172,6 +203,7 @@ func (item *MultiGetItem) VersionType(versionType string) *MultiGetItem {
 	return item
 }
 
+// FetchSource allows to specify source filtering.
 func (item *MultiGetItem) FetchSource(fetchSourceContext *FetchSourceContext) *MultiGetItem {
 	item.fsc = fetchSourceContext
 	return item
@@ -197,11 +229,11 @@ func (item *MultiGetItem) Source() (interface{}, error) {
 		}
 		source["_source"] = src
 	}
-	if item.fields != nil {
-		source["fields"] = item.fields
-	}
 	if item.routing != "" {
 		source["_routing"] = item.routing
+	}
+	if len(item.storedFields) > 0 {
+		source["stored_fields"] = strings.Join(item.storedFields, ",")
 	}
 	if item.version != nil {
 		source["version"] = fmt.Sprintf("%d", *item.version)
@@ -215,6 +247,7 @@ func (item *MultiGetItem) Source() (interface{}, error) {
 
 // -- Result of a Multi Get request.
 
+// MgetResponse is the outcome of a Multi GET API request.
 type MgetResponse struct {
 	Docs []*GetResult `json:"docs,omitempty"`
 }
