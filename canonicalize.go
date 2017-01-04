a80af35aa41856dc2c986204e2b64eab81ccac3a
Oliver Eilhard <oliver@eilhard.net>
2017-01-04 12:10:34 +0100

    Be more strict in accepted client URLs
    Go 1.8 does more strict URL checks. E.g. Go 1.7 accepted to parse the
    URL `"127.0.0.1:9200"` whereas Go 1.8 will raise an error: "first path
    segment in URL cannot contain colon".
    
    We use that as a chance to also be more strict about what is considered
    a valid URL. Until now, we accepted strings like `"127.0.0.1"` or
    `"127.0.0.1:9200"`. Those are invalid URLs in the first place, and we
    now treat them as such.
    
    Close #429

--- canonicalize.go
+++ canonicalize.go
@@ -8,21 +8,17 @@ import "net/url"
 
 // canonicalize takes a list of URLs and returns its canonicalized form, i.e.
 // remove anything but scheme, userinfo, host, path, and port.
-// It also removes all trailing slashes. It also skips invalid URLs or
-// URLs that do not use protocol http or https.
+// It also removes all trailing slashes. Invalid URLs or URLs that do not
+// use protocol http or https are skipped.
 //
 // Example:
 // http://127.0.0.1:9200/?query=1 -> http://127.0.0.1:9200
 // http://127.0.0.1:9200/db1/ -> http://127.0.0.1:9200/db1
-// 127.0.0.1:9200 -> http://127.0.0.1:9200
 func canonicalize(rawurls ...string) []string {
 	var canonicalized []string
 	for _, rawurl := range rawurls {
 		u, err := url.Parse(rawurl)
 		if err == nil {
-			if len(u.Scheme) == 0 {
-				u.Scheme = DefaultScheme
-			}
 			if u.Scheme == "http" || u.Scheme == "https" {
 				// Trim trailing slashes
 				for len(u.Path) > 0 && u.Path[len(u.Path)-1] == '/' {
