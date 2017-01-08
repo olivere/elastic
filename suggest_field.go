9b54cb385a0fc4e38d4807ec2b426aeebb9b766c
Oliver Eilhard <oliver@eilhard.net>
2017-01-08 19:06:07 +0100

    Suggester Context queries should be passed as a map
    The documentation for [Context
    Suggester](https://www.elastic.co/guide/en/elasticsearch/reference/5.0/suggester-context.html)
    specifies that context queries should be passed as a map, not an array.
    This is true for both 2.x as well as 5.x.
    
    See #408

--- suggest_field.go
+++ suggest_field.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2015 Oliver Eilhard. All rights reserved.
+// Copyright 2012-present Oliver Eilhard. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
@@ -6,6 +6,7 @@ package elastic
 
 import (
 	"encoding/json"
+	"errors"
 )
 
 // SuggestField can be used by the caller to specify a suggest field
@@ -85,13 +86,19 @@ func (f *SuggestField) MarshalJSON() ([]byte, error) {
 		}
 		source["context"] = src
 	default:
-		var ctxq []interface{}
+		ctxq := make(map[string]interface{})
 		for _, query := range f.contextQueries {
 			src, err := query.Source()
 			if err != nil {
 				return nil, err
 			}
-			ctxq = append(ctxq, src)
+			m, ok := src.(map[string]interface{})
+			if !ok {
+				return nil, errors.New("SuggesterContextQuery must be of type map[string]interface{}")
+			}
+			for k, v := range m {
+				ctxq[k] = v
+			}
 		}
 		source["context"] = ctxq
 	}
--- suggest_field_test.go
+++ suggest_field_test.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2015 Oliver Eilhard. All rights reserved.
+// Copyright 2012-present Oliver Eilhard. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
@@ -12,7 +12,6 @@ import (
 func TestSuggestField(t *testing.T) {
 	field := NewSuggestField().
 		Input("Welcome to Golang and Elasticsearch.", "Golang and Elasticsearch").
-		Output("Golang and Elasticsearch: An introduction.").
 		Weight(1).
 		ContextQuery(
 			NewSuggesterCategoryMapping("color").FieldName("color_field").DefaultValues("red", "green", "blue"),
@@ -23,7 +22,7 @@ func TestSuggestField(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"context":[{"color":{"default":["red","green","blue"],"path":"color_field","type":"category"}},{"location":{"default":{"lat":52.516275,"lon":13.377704},"neighbors":true,"precision":["5m"],"type":"geo"}}],"input":["Welcome to Golang and Elasticsearch.","Golang and Elasticsearch"],"output":"Golang and Elasticsearch: An introduction.","weight":1}`
+	expected := `{"context":{"color":{"default":["red","green","blue"],"path":"color_field","type":"category"},"location":{"default":{"lat":52.516275,"lon":13.377704},"neighbors":true,"precision":["5m"],"type":"geo"}},"input":["Welcome to Golang and Elasticsearch.","Golang and Elasticsearch"],"weight":1}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
