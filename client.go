9e1b4c7c6fff284621263ee9fed1e053886b8dc2
Oliver Eilhard <oliver@eilhard.net>
2017-01-28 19:40:45 +0100

    Prepare release v3.0.64

--- client.go
+++ client.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "3.0.63"
+	Version = "3.0.64"
 
 	// DefaultUrl is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
