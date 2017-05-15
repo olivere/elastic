760dc253dbf272b413d112f5af4d83221664b3a4
Oliver Eilhard <oliver@eilhard.net>
2017-05-15 17:40:42 +0200

    Use `value` instead of `prefix` in Prefix Query
    See #526

--- search_queries_prefix_test.go
+++ search_queries_prefix_test.go
@@ -2,15 +2,42 @@
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
-package elastic
+package elastic_test
 
 import (
+	"context"
 	"encoding/json"
 	"testing"
+
+	"gopkg.in/olivere/elastic.v5"
 )
 
+func ExamplePrefixQuery() {
+	// Get a client to the local Elasticsearch instance.
+	client, err := elastic.NewClient()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// Define wildcard query
+	q := elastic.NewPrefixQuery("user", "oli")
+	q = q.QueryName("my_query_name")
+
+	searchResult, err := client.Search().
+		Index("twitter").
+		Query(q).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	_ = searchResult
+}
+
 func TestPrefixQuery(t *testing.T) {
-	q := NewPrefixQuery("user", "ki")
+	q := elastic.NewPrefixQuery("user", "ki")
 	src, err := q.Source()
 	if err != nil {
 		t.Fatal(err)
@@ -27,7 +54,7 @@ func TestPrefixQuery(t *testing.T) {
 }
 
 func TestPrefixQueryWithOptions(t *testing.T) {
-	q := NewPrefixQuery("user", "ki")
+	q := elastic.NewPrefixQuery("user", "ki")
 	q = q.QueryName("my_query_name")
 	src, err := q.Source()
 	if err != nil {
@@ -38,7 +65,7 @@ func TestPrefixQueryWithOptions(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"prefix":{"user":{"_name":"my_query_name","prefix":"ki"}}}`
+	expected := `{"prefix":{"user":{"_name":"my_query_name","value":"ki"}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
