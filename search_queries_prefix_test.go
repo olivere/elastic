3f49414dc29d45ba96fde19ba72bc646077dbffd
Oliver Eilhard <oliver@eilhard.net>
2017-05-15 17:52:10 +0200

    Split prefix tests and example

--- search_queries_prefix_test.go
+++ search_queries_prefix_test.go
@@ -2,42 +2,15 @@
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
-package elastic_test
+package elastic
 
 import (
-	"context"
 	"encoding/json"
 	"testing"
-
-	"gopkg.in/olivere/elastic.v5"
 )
 
-func ExamplePrefixQuery() {
-	// Get a client to the local Elasticsearch instance.
-	client, err := elastic.NewClient()
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-
-	// Define wildcard query
-	q := elastic.NewPrefixQuery("user", "oli")
-	q = q.QueryName("my_query_name")
-
-	searchResult, err := client.Search().
-		Index("twitter").
-		Query(q).
-		Pretty(true).
-		Do(context.Background())
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-	_ = searchResult
-}
-
 func TestPrefixQuery(t *testing.T) {
-	q := elastic.NewPrefixQuery("user", "ki")
+	q := NewPrefixQuery("user", "ki")
 	src, err := q.Source()
 	if err != nil {
 		t.Fatal(err)
@@ -54,7 +27,7 @@ func TestPrefixQuery(t *testing.T) {
 }
 
 func TestPrefixQueryWithOptions(t *testing.T) {
-	q := elastic.NewPrefixQuery("user", "ki")
+	q := NewPrefixQuery("user", "ki")
 	q = q.QueryName("my_query_name")
 	src, err := q.Source()
 	if err != nil {
