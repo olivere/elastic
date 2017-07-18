99e76ec837f15ae5d495579fe10671556384faae
Oliver Eilhard <oliver@eilhard.net>
2017-07-18 08:29:05 +0200

    Trace HTTP responses even in the case of errors
    This closes long standing issues #297 and #553.

--- client.go
+++ client.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "5.0.41"
+	Version = "5.0.42"
 
 	// DefaultURL is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
