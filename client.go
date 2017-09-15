8d03650b62e8f6209a24672e692979e5dcd60fda
Oliver Eilhard <oliver@eilhard.net>
2017-09-15 19:30:59 +0200

    Prepare update to 5.0.46

--- client.go
+++ client.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "5.0.45"
+	Version = "5.0.46"
 
 	// DefaultURL is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
