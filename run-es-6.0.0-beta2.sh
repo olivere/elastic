386aef4c9bd34c3e481c8e00f1f592acf65f905d
Oliver Eilhard <oliver@eilhard.net>
2017-11-15 09:33:08 +0100

    All tests green on 6.0.0 GA

--- run-es-6.0.0-beta2.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -p 9200:9200  -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "bootstrap.memory_lock=true" -e "ES_JAVA_OPTS=-Xms1g -Xmx1g" docker.elastic.co/elasticsearch/elasticsearch:6.0.0-beta2 elasticsearch -Expack.security.enabled=false -Enetwork.host=_local_,_site_ -Enetwork.publish_host=_local_
