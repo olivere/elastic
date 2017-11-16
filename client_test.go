426df06bd62ca68d4dd857681341b8069af25257
Oliver Eilhard <oliver@eilhard.net>
2017-11-16 10:05:14 +0100

    Fix test

--- client_test.go
+++ client_test.go
@@ -279,8 +279,8 @@ func TestClientHealthcheckStartupTimeout(t *testing.T) {
 	if !IsConnErr(err) {
 		t.Fatal(err)
 	}
-	if want, have := "health check timeout: Head http://localhost:9299: dial tcp [::1]:9299: getsockopt: connection refused: no Elasticsearch node available", err.Error(); want != have {
-		t.Fatalf("want %q, have %q", want, have)
+	if !strings.Contains(err.Error(), "connection refused") {
+		t.Fatalf("expected error to contain %q, have %q", "connection refused", err.Error())
 	}
 	if duration < 5*time.Second {
 		t.Fatalf("expected a timeout in more than 5 seconds; got: %v", duration)
