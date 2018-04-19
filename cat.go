202ae9be5a8f79a62ede5ff220b5b1a68865acc8
Justin Beal <jbeal@digitalocean.com>
2018-04-19 13:42:34 -0400

    Support multiple model types for cat

--- cat.go
+++ cat.go
@@ -6,10 +6,11 @@ package elastic
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 )
 
-// CatIndicesService executes cat commands against the cluster.
+// CatService executes cat commands against the cluster.
 //
 // See https://www.elastic.co/guide/en/elasticsearch/reference/6.2/cat.html
 // for details.
@@ -17,6 +18,46 @@ type CatService struct {
 	client *Client
 	pretty bool
 	model  string
+}
+
+const (
+	aliases      string = "aliases"
+	allocation   string = "allocation"
+	count        string = "count"
+	fielddata    string = "fielddata"
+	health       string = "health"
+	indices      string = "indices"
+	master       string = "master"
+	nodeattrs    string = "nodeattrs"
+	nodes        string = "nodes"
+	pendingtasks string = "pending_tasks"
+	plugins      string = "plugins"
+	recovery     string = "recovery"
+	repositories string = "repositories"
+	shards       string = "shards"
+	segments     string = "segments"
+	templates    string = "templates"
+	threadpool   string = "thread_pool"
+)
+
+var catModels = map[string]struct{}{
+	aliases:      struct{}{},
+	allocation:   struct{}{},
+	count:        struct{}{},
+	fielddata:    struct{}{},
+	health:       struct{}{},
+	indices:      struct{}{},
+	master:       struct{}{},
+	nodeattrs:    struct{}{},
+	nodes:        struct{}{},
+	pendingtasks: struct{}{},
+	plugins:      struct{}{},
+	recovery:     struct{}{},
+	repositories: struct{}{},
+	shards:       struct{}{},
+	segments:     struct{}{},
+	templates:    struct{}{},
+	threadpool:   struct{}{},
 }
 
 // NewCatService creates a new CatService.
@@ -27,7 +68,105 @@ func NewCatService(client *Client) *CatService {
 	}
 }
 
+// Aliases sets the service to show information about currently configured aliases to indices including filter and routing infos.
+func (s *CatService) Aliases() *CatService {
+	s.model = aliases
+	return s
+}
+
+// Allocation sets the service to show a snapshot of how many shards are allocated to each data node and how much disk space they are using.
+func (s *CatService) Allocation() *CatService {
+	s.model = allocation
+	return s
+}
+
+// Count sets the service to show the document count of the entire cluster, or individual indices.
+func (s *CatService) Count() *CatService {
+	s.model = count
+	return s
+}
+
+// FieldData sets the service to show how much heap memory is currently being used by fielddata on every data node in the cluster.
+func (s *CatService) FieldData() *CatService {
+	s.model = fielddata
+	return s
+}
+
+// Health sets the service to show a terse, one-line representation of the same information from /_cluster/health
+func (s *CatService) Health() *CatService {
+	s.model = health
+	return s
+}
+
+// Indices sets the service to show a cross-section of each index.
 func (s *CatService) Indices() *CatService {
+	s.model = indices
+	return s
+}
+
+// Master sets the service to show the master’s node ID, bound IP address, and node name.
+func (s *CatService) Master() *CatService {
+	s.model = master
+	return s
+}
+
+// NodeAttrs sets the service to show custom node attributes.
+func (s *CatService) NodeAttrs() *CatService {
+	s.model = nodeattrs
+	return s
+}
+
+// Nodes sets the service to show the cluster topology.
+func (s *CatService) Nodes() *CatService {
+	s.model = nodes
+	return s
+}
+
+// PendingTasks sets the service to show data about any pending updates to the cluster state.
+func (s *CatService) PendingTasks() *CatService {
+	s.model = pendingtasks
+	return s
+}
+
+// Plugins sets the service to show a view per node of running plugins.
+func (s *CatService) Plugins() *CatService {
+	s.model = plugins
+	return s
+}
+
+// Recovery sets the service to show a view of index shard recoveries, both on-going and previously completed.
+func (s *CatService) Recovery() *CatService {
+	s.model = recovery
+	return s
+}
+
+// Repositories sets the service to show the snapshot repositories registered in the cluster.
+func (s *CatService) Repositories() *CatService {
+	s.model = repositories
+	return s
+}
+
+// Segments sets the service to show low level information about the segments in the shards of an index.
+func (s *CatService) Segments() *CatService {
+	s.model = segments
+	return s
+}
+
+// Shards sets the service to show the detailed view of what nodes contain which shards.
+func (s *CatService) Shards() *CatService {
+	s.model = shards
+	return s
+}
+
+// Templates sets the service to show information about existing templates.
+func (s *CatService) Templates() *CatService {
+	s.model = templates
+	return s
+}
+
+// ThreadPool sets the service to show cluster wide thread pool statistics per node.
+func (s *CatService) ThreadPool() *CatService {
+	s.model = threadpool
 	return s
 }
 
@@ -39,7 +178,7 @@ func (s *CatService) Pretty(pretty bool) *CatService {
 
 // buildURL builds the URL for the operation.
 func (s *CatService) buildURL() (string, url.Values, error) {
-	path := "/_cat/indices"
+	path := fmt.Sprintf("/_cat/%s", s.model)
 
 	// Add query string parameters
 	params := url.Values{}
@@ -52,6 +191,14 @@ func (s *CatService) buildURL() (string, url.Values, error) {
 
 // Validate checks if the operation is valid.
 func (s *CatService) Validate() error {
+	if s.model == "" {
+		return fmt.Errorf("no model specified for cat command")
+	}
+
+	if _, ok := catModels[s.model]; !ok {
+		return fmt.Errorf("unknown model '%s' specified for cat command", s.model)
+	}
+
 	return nil
 }
 
--- cat_test.go
+++ cat_test.go
@@ -6,18 +6,74 @@ package elastic
 
 import (
 	"context"
+	"fmt"
 	"testing"
 )
 
-func TestCatIndices(t *testing.T) {
+func TestCatModels(t *testing.T) {
 	client := setupTestClientAndCreateIndex(t)
 
-	res, err := client.Cat().Indices().Pretty(true).Do(context.TODO())
-	if err != nil {
-		t.Fatalf("expected to not get an error, got %v", err)
+	c := client.Cat()
+
+	tests := []struct {
+		f    func() *CatService
+		name string
+	}{
+		{f: c.Aliases, name: "aliases"},
+		{f: c.Allocation, name: "allocation"},
+		{f: c.Count, name: "count"},
+		{f: c.FieldData, name: "fielddata"},
+		{f: c.Health, name: "health"},
+		{f: c.Indices, name: "indices"},
+		{f: c.Master, name: "master"},
+		{f: c.NodeAttrs, name: "nodeattrs"},
+		{f: c.Nodes, name: "nodes"},
+		{f: c.PendingTasks, name: "pendingtasks"},
+		{f: c.Plugins, name: "plugins"},
+		{f: c.Recovery, name: "recovery"},
+		{f: c.Repositories, name: "repositories"},
+		{f: c.Shards, name: "shards"},
+		{f: c.Segments, name: "segments"},
+// TODO - it is not possible to cat snapshots where no repo exists.
+// Creating one for the purpose of this test fails without setting the path.repo or repositories.url.allowed_url setting .
+// Putting this off to a later issue.
+		{f: c.Templates, name: "templates"},
+		{f: c.ThreadPool, name: "threadpool"},
 	}
 
-	if res == "" {
-		t.Fatalf("expected res to not be an empty string")
+	for _, tt := range tests {
+		model := tt.f
+		res, err := model().Do(context.TODO())
+
+		if err != nil {
+			t.Errorf("model %s - expected to not get an error, got %v", tt.name, err)
+		}
+
+		if res == "" {
+			t.Errorf("model %s - expected res to not be an empty string", tt.name)
+		}
+	}
+}
+
+func TestCatUnknownModel(t *testing.T) {
+	client := setupTestClientAndCreateIndex(t)
+
+	c := client.Cat()
+	c.model = "boogers"
+
+	_, err := c.Do(context.TODO())
+
+	if err == nil {
+		t.Fatalf("expected to get an error, but did not", err)
+	}
+}
+
+func TestCatNoModel(t *testing.T) {
+	client := setupTestClientAndCreateIndex(t)
+
+	_, err := client.Cat().Do(context.TODO())
+
+	if err == nil {
+		t.Fatalf("expected to get an error, but did not", err)
 	}
 }
