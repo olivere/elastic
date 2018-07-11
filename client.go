d282b02aca3f0dfc97aac6605808beafc3a4d03d
Oliver Eilhard <oliver@eilhard.net>
2018-07-11 13:29:11 +0200

    Ignore temporary failures for url.Error
    This commit ignores *url.Error errors that are marked as temporary. It
    also removes the context timeout checks for retrier added earlier
    because that cannot happen in that code path.
    
    Fix #484

--- client.go
+++ client.go
@@ -1307,7 +1307,7 @@ func (c *Client) PerformRequestWithOptions(ctx context.Context, opt PerformReque
 		}
 		if ue, ok := err.(*url.Error); ok {
 			// This happens e.g. on redirect errors, see https://golang.org/src/net/http/client_test.go#L329
-			if ue.Err == context.Canceled || ue.Err == context.DeadlineExceeded {
+			if ue.Err == context.Canceled || ue.Err == context.DeadlineExceeded || ue.Temporary() {
 				// Proceed, but don't mark the node as dead
 				return nil, err
 			}
