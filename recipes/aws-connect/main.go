61c2207261530d4ef70946b336a856123b1667b6
Oliver Eilhard <oliver@eilhard.net>
2018-07-28 17:10:50 +0200

    Include AWS recipe now that 5.0.71 is released

--- recipes/aws-connect/main.go
+++ recipes/aws-connect/main.go
@@ -1,4 +1,3 @@
-/*
 // Copyright 2012-present Oliver Eilhard. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
@@ -61,4 +60,3 @@ func main() {
 	// Just a status message
 	fmt.Println("Connection succeeded")
 }
-*/
