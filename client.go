3b4280ae88ae97478b29ad5e4c868cf7242b61f1
Oliver Eilhard <oliver@eilhard.net>
2018-08-28 10:27:43 +0200

    Fixing issues with go.mod

--- client.go
+++ client.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "6.2.1"
+	Version = "6.2.2"
 
 	// DefaultURL is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
@@ -452,9 +452,11 @@ func configToOptions(cfg *config.Config) ([]ClientOptionFunc, error) {
 		if cfg.Sniff != nil {
 			options = append(options, SetSniff(*cfg.Sniff))
 		}
+		/*
 		if cfg.Healthcheck != nil {
 			options = append(options, SetHealthcheck(*cfg.Healthcheck))
 		}
+		*/
 	}
 	return options, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/olivere/elastic/v6
 require (
 	github.com/fortytw2/leaktest v1.2.0
 	github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329
+	github.com/olivere/elastic v6.1.25+incompatible
 	github.com/opentracing/opentracing-go v1.0.2
 	github.com/pkg/errors v0.8.0
 	github.com/smartystreets/go-aws-auth v0.0.0-20180515143844-0c1422d1fdb9
