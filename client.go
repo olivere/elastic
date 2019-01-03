f5738570b28a4b5fab39442e3a5b5ffc599a562c
Oliver Eilhard <oliver@eilhard.net>
2019-01-03 13:43:50 +0100

    Release 6.2.15

--- client.go
+++ client.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "6.2.14"
+	Version = "6.2.15"
 
 	// DefaultURL is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
