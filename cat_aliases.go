cf412da37aa085b2966133c2cd698f79ebf70ed7
dolauli <dolauli@hotmail.com>
2019-08-15 22:12:22 +0800

    asd

--- cat_aliases.go
+++ cat_aliases.go
@@ -1,5 +1,5 @@
 // Copyright 2012-present Oliver Eilhard. All rights reserved.
-// Use of this source code is governed by a MIT-license.
+// Use of this source code is governed by a MIT-license. 
 // See http://olivere.mit-license.org/license.txt for details.
 
 package elastic
