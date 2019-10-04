d41fa8c4dc2dbf6edd80fb25b6cc3e0a8a71d461
Oliver Eilhard <oliver@eilhard.net>
2019-10-04 18:19:53 +0200

    Add ScriptScoreQuery
    This commit adds Script Score Query as documented at
    https://www.elastic.co/guide/en/elasticsearch/reference/7.4/query-dsl-script-score-query.html.
    
    Close #1197

---
+++ search_queries_script_score_test.go
@@ -0,0 +1,79 @@
+// Copyright 2012-present Oliver Eilhard. All rights reserved.
+// Use of this source code is governed by a MIT-license.
+// See http://olivere.mit-license.org/license.txt for details.
+
+package elastic
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+)
+
+func TestScriptScoreQuery(t *testing.T) {
+	q := NewScriptScoreQuery().
+		Query(NewMatchQuery("message", "elasticsearch")).
+		Script(NewScript("doc['likes'].value / 10")).
+		MinScore(1.1).
+		QueryName("my_query")
+	src, err := q.Source()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"script_score":{"_name":"my_query","min_score":1.1,"query":{"match":{"message":{"query":"elasticsearch"}}},"script":{"source":"doc['likes'].value / 10"}}}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
+
+func TestScriptScoreQueryIntegration(t *testing.T) {
+	client := setupTestClientAndCreateIndexAndAddDocs(t) //, SetTraceLog(log.New(os.Stdout, "", 0)))
+
+	ssq := NewScriptScoreQuery().
+		Query(NewMatchQuery("message", "Golang")).
+		Script(NewScript("(1 + doc['retweets'].value) * 10"))
+	res, err := client.Search().
+		Index(testIndexName).
+		Query(ssq).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.Hits == nil {
+		t.Errorf("expected Hits != nil; got nil")
+	}
+	if want, have := int64(1), res.TotalHits(); want != have {
+		t.Errorf("expected TotalHits() = %d; got %d", want, have)
+	}
+	if want, have := 1, len(res.Hits.Hits); want != have {
+		t.Errorf("expected len(Hits.Hits) = %d; got %d", want, have)
+	}
+
+	hit := res.Hits.Hits[0]
+
+	if want, have := testIndexName, hit.Index; want != have {
+		t.Fatalf("expected Hits.Hit.Index = %q; got %q", want, have)
+	}
+	if want, have := "1", hit.Id; want != have {
+		t.Fatalf("expected Hits.Hit.Id = %q; got %q", want, have)
+	}
+	if hit.Score == nil {
+		t.Fatal("expected Hits.Hit.Score != nil")
+	}
+	if want, have := 10.0, *hit.Score; want != have {
+		t.Fatalf("expected Hits.Hit.Score = %v; got %v", want, have)
+	}
+	var tw tweet
+	if err := json.Unmarshal(hit.Source, &tw); err != nil {
+		t.Fatal(err)
+	}
+	if want, have := "olivere", tw.User; want != have {
+		t.Fatalf("expected User = %q; got %q", want, have)
+	}
+}
