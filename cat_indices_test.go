739bb3a784fd6b5cbc78d5d85c72f63b66cc8ce1
Evghenii Maslennikov <evghenii.maslennikov@gmail.com>
2019-11-17 01:34:45 +0200

    feature: cat indices. aliases for columns

--- cat_indices_test.go
+++ cat_indices_test.go
@@ -26,3 +26,57 @@ func TestCatIndices(t *testing.T) {
 		t.Fatalf("Index[0]: want != %q, have %q", "", have)
 	}
 }
+
+// TestCatIndicesResponseRowAliasesMap tests if catIndicesResponseRowAliasesMap was inited
+func TestCatIndicesResponseRowAliasesMap(t *testing.T) {
+	if catIndicesResponseRowAliasesMap == nil {
+		t.Fatal("want catIndicesResponseRowAliasesMap to be not nil")
+	}
+
+	if len(catIndicesResponseRowAliasesMap) == 0 {
+		t.Fatal("want catIndicesResponseRowAliasesMap to be not empty")
+	}
+}
+
+// TestCatIndicesWithAliases makes a simple test (if ?h=h will be the same as ?h=health)
+func TestCatIndicesWithAliases(t *testing.T) {
+	client := setupTestClientAndCreateIndexAndAddDocs(t, SetDecoder(&strictDecoder{})) // , SetTraceLog(log.New(os.Stdout, "", 0)))
+	ctx := context.Background()
+	res, err := client.CatIndices().Columns("h").Do(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res == nil {
+		t.Fatal("want response, have nil")
+	}
+	if len(res) == 0 {
+		t.Fatalf("want response, have: %v", res)
+	}
+	if have := res[0].Health; have == "" {
+		t.Fatalf("Index[0]: want != %q, have %q", "", have)
+	}
+}
+
+// TestCatIndicesWithAliases makes a test with a double-alias
+// asking `?h=rti` will fill one of the refresh.external_time/refresh.time fields (depending on elasticsearch version)
+func TestCatIndicesWithAliases_Double(t *testing.T) {
+	client := setupTestClientAndCreateIndexAndAddDocs(t, SetDecoder(&strictDecoder{})) // , SetTraceLog(log.New(os.Stdout, "", 0)))
+	ctx := context.Background()
+	res, err := client.CatIndices().Columns("rti").Do(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res == nil {
+		t.Fatal("want response, have nil")
+	}
+	if len(res) == 0 {
+		t.Fatalf("want response, have: %v", res)
+	}
+
+	refreshTime := res[0].RefreshTime
+	refreshExternalTime := res[0].RefreshExternalTime
+
+	if refreshTime == "" && refreshExternalTime == "" {
+		t.Fatalf("Index[0]: want one of [refreshTime or refreshExternalTime] be not empty")
+	}
+}
