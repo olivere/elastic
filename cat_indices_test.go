e56a04d402d31d541a7975c049a385f8479fc902
Oliver Eilhard <oliver@eilhard.net>
2020-01-04 15:02:41 +0100

    Add column aliases in cat indices API
    This commit allows to specify aliases to the cat indices API,
    in the same way that the `curl` command does when using
    the `_cat/indices` endpoint.
    
    See #1236

--- cat_indices_test.go
+++ cat_indices_test.go
@@ -26,3 +26,33 @@ func TestCatIndices(t *testing.T) {
 		t.Fatalf("Index[0]: want != %q, have %q", "", have)
 	}
 }
+
+// TestCatIndicesResponseRowAliasesMap tests if catIndicesResponseRowAliasesMap is declared
+func TestCatIndicesResponseRowAliasesMap(t *testing.T) {
+	if catIndicesResponseRowAliasesMap == nil {
+		t.Fatal("want catIndicesResponseRowAliasesMap to be not nil")
+	}
+
+	if len(catIndicesResponseRowAliasesMap) == 0 {
+		t.Fatal("want catIndicesResponseRowAliasesMap to be not empty")
+	}
+}
+
+// TestCatIndicesWithAliases makes a simple test (if ?h=h will be the same as ?h=health)
+func TestCatIndicesWithAliases(t *testing.T) {
+	client := setupTestClientAndCreateIndexAndAddDocs(t, SetDecoder(&strictDecoder{})) // , SetTraceLog(log.New(os.Stdout, "", 0)))
+	ctx := context.Background()
+	res, err := client.CatIndices().Columns("h").Do(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res == nil {
+		t.Fatal("want response, have nil")
+	}
+	if len(res) == 0 {
+		t.Fatalf("want response, have: %v", res)
+	}
+	if have := res[0].Health; have == "" {
+		t.Fatalf("Index[0]: want != %q, have %q", "", have)
+	}
+}
