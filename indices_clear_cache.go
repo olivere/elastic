e2592ec1af8f0723089fac5456c2e209c7776968
Aman Jain <amanjain5221@gmail.com>
2020-02-17 19:23:14 +0900

    Update copyright

--- indices_clear_cache.go
+++ indices_clear_cache.go
@@ -1,4 +1,4 @@
-// Copyright 2020-present Aman Jain. All rights reserved.
+// Copyright 2012-present Oliver Eilhard. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
