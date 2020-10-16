18750adc9023f5ba36332e61772fb05de7b18d95
Oliver Eilhard <oliver@eilhard.net>
2020-10-16 17:23:28 +0200

    Allow collapsing on inner hits
    As described in
    https://www.elastic.co/guide/en/elasticsearch/reference/7.9/collapse-search-results.html#second-level-of-collapsing,
    collapsing is possible on the second level as well, on the inner hit.
    
    Close #1394
    Close #1401

--- inner_hit_test.go
+++ inner_hit_test.go
@@ -42,3 +42,20 @@ func TestInnerHitWithName(t *testing.T) {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
 }
+
+func TestInnerHitSecondLevelCollapse(t *testing.T) {
+	hit := NewInnerHit().Name("by_location").Size(3).Collapse(NewCollapseBuilder("user.id"))
+	src, err := hit.Source()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"collapse":{"field":"user.id"},"name":"by_location","size":3}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
